5074a0dd6a32eaaff1f5a78ec8a7a9221e26c5c9
sergeyemelianov <semelianov@domprog.com>
2024-06-15 02:37:42 +0300

    v6 - minor updates

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
 import express from 'express';
 import http from 'http';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 const app = express();
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 app.use(express.static('./dist'));
-app.use(express.static('./src'));
-app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, './dist/index.html'))
-});
 
 server.listen(port, function () {
-    console.log(`===> Listening on port ${port}!`);
+    console.log(`Server on port ${port}!`);
 });
