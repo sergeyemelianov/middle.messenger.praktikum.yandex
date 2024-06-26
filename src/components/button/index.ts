2ea784dd9f2673460baa8fbcbc8f25f5d7faa286
sergeyemelianov <semelianov@domprog.com>
2024-06-26 11:52:26 +0300

    v2 - core init - eventbus, block, http

--- src/components/button/index.ts
+++ src/components/button/index.ts
@@ -1,3 +1,23 @@
 import './button-component.scss';
+import Block from '../../core/Block';
 
-export { default as Button } from './button-component.hbs?raw';
+import ButtonTemplate from './button-component.hbs?raw';
+
+type ButtonProps = {
+  type?: string;
+  selector?: string;
+  page?: string;
+  label?: string;
+  iconName?: string;
+  iconSize?: string;
+}
+
+export default class Button extends Block {
+  constructor(props: ButtonProps) {
+    super({ ...props });
+  }
+
+  render(): string {
+    return ButtonTemplate;
+  }
+}
