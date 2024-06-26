9e4512d7ec5cf9f2e6615e0e7d5f1ba7a70cd2af
sergeyemelianov <semelianov@domprog.com>
2024-06-26 12:07:29 +0300

    v2 - core init - eventbus, block, http

--- src/components/input/index.ts
+++ src/components/input/index.ts
@@ -1,3 +1,43 @@
 import './input-component.scss';
+import Block from '../../core/Block';
 
-export { default as Input } from './input-component.hbs?raw';
+import InputTemplate from './input-component.hbs?raw';
+
+type InputProps = {
+  showLabel?: boolean;
+  name?: string;
+  label?: string;
+  selector: string;
+  type?: string;
+  value?: string;
+  pattern?: string;
+  placeholder?: string;
+  readonly?: boolean;
+  autofocus?: boolean;
+  required?: boolean;
+  minL?: string;
+  max?: string;
+  showDivider?: boolean;
+  page?: string;
+  onChange: (value: string) => void;
+};
+
+export default class Input extends Block {
+  constructor(props: InputProps) {
+    super({
+      ...props,
+      events: {
+        change: (e) => props.onChange(e.target.value),
+        blur: (e) => this.validate(e.target.value),
+      },
+    });
+  }
+
+  render(): string {
+    return InputTemplate;
+  }
+
+  validate(value: string): void {
+    console.log('validate', value);
+  }
+}
