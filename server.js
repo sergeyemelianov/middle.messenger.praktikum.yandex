1fc4cf7ee5725a34fe495db42ece282811b2505a
Sergey Emelianov <semelianov@domprog.com>
2024-08-02 15:22:23 +0300

    s3 - fix

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import http from 'http';
-
+import { fileURLToPath } from 'url';
 const app = express();
 const port = process.env.PORT || 3000;
+import path from 'path';
 const server = http.createServer(app);
 
-app.use(express.static('./dist'));
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.use(express.static(__dirname + '/dist'));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/dist/index.html"));
+});
 server.listen(port, () => {
     console.log(`Server on port ${port}!`);
 });
